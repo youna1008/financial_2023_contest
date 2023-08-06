febb72bfe0d95df61ac3a31a03b417064670ab79
20171609 <clw3wb@naver.com>
2023-08-07 01:27:54 +0900

    modify : add mid, small filter

--- graph.py
+++ graph.py
@@ -8,12 +8,13 @@ class Graph():
         self.min_count = min_count
         self.max_group = max_group
         self.big_group_result = set()
-        self.f_index = dict()
-
+        self.mid_group_result = set()
+        self.small_group_result = set()
+        self.f_filter = dict()
 
     def filter_graph(self, feature, index=None):
         # 각 feature에 대한 인덱스 집합
-        self.f_index = dict()
+        self.f_filter = dict()
         if index:
             data = self.data[self.data['index'] == index]
         
@@ -32,16 +33,19 @@ class Graph():
                 continue
 
             for value, count in value_counts.items():
-                self.f_index[f'{column}_{value}'] = set(data[data[column] == value]['index'])
+                self.f_filter[f'{column}_{value}'] = set(data[data[column] == value]['index'])
 
-        return self.f_index
+        return self.f_filter
     
-    def merge_f_graph(self, feature):
+    # 특정 index에 대해서 다시한번 진행 (얘는 min을 적용 안하나?)
+    def merge_f_graph(self, feature, t):
         temp = dict()
         c_list = list()
 
-        group_list = self.f_index.keys()
+        group_list = self.f_filter.keys()
+        total_group_result = set()
 
+        # cls_set은 사용하는 클래스들을 분류
         for group in group_list:
             cls_set = set()
             data = self.data[self.data['index'].isin(self.index[group])]
@@ -61,6 +65,55 @@ class Graph():
                 
             # 각각 어캐 담아야할지 고민해봐야 함.
             # 앞에는 중분류로 가기위함, 뒤에는 그룹의 총 묶인거, 마지막은 인덱스
+            # 중복제거 한번 해줘야하나 싶기도 함
             group_result = '-'.join(c_list)
-            self.big_group_result.add((cls_set, group_result, self.f_index[group]))
+            total_group_result.add((cls_set, group_result, self.f_index[group]))
 
+        if t == "big":
+            self.big_group_result = total_group_result
+        if t == "mid":
+            self.mid_group_result = total_group_result
+        if t == "small":
+            self.small_group_result = total_group_result
+
+    # f_filter는 temp데이터여서 없애는 방식 도입할 수 있을듯
+    # mid랑 small이랑 big이 대입하는게 거의 비슷해서 같은 함수로 사용할 수 있을지도?
+    # 내일 얘기해보면서 코드 작성하기?
+    def mid_group_filter(self):
+        for cls, group_key, idx in self.big_group_result:
+            # mid그룹의 column가져오기
+            columns = self.get_key(cls, "mid")
+
+            filter_data = self.filter_graph(columns, idx)
+            self.merge_f_graph(columns, "mid")
+
+    def small_group_filter(self):
+        for cls, group_key, idx in self.mid_group_result:
+            # mid그룹의 column가져오기
+            columns = self.get_key(cls, "small")
+
+            filter_data = self.filter_graph(columns, idx)
+            self.merge_f_graph(columns, "small")
+
+
+
+    def get_key(self, cls, t):
+        result = set()
+        big = self.class_dict.keys()
+        for b in big:
+            if t == "big":
+                if b in cls:
+                    result.add(b)
+ 
+            mid = self.class_dict[b].keys()
+            for m in mid:
+                if t == "mid":
+                    if m in cls:
+                        result.add(m)
+
+                if t == "small":
+                    for s in self.class_dict[b][m]:
+                        if s in cls:
+                            result.add(s)
+        
+        return result
